ad64fc13c028dda0bfe60d28b58dfc5e9d5e25ea
Juuso Lehtinen <juuso@karttalehtinen.fi>
2020-07-17 17:39:29 +0300

    Add linting rules, fix code style errors
    Add husky for pre-push validation.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 // qth-locator  Copyright 2019 Juuso Lehtinen https://github.com/jleh/qth-locator/  MIT License
-"use strict";
+'use strict';
 
 const CHAR_CODE_OFFSET = 65;
 
 const isValidLocatorString = locatorString => locatorString.match(/^[A-R][A-R]\d\d[a-x][a-x]/) !== null;
 
-const charToNumber = (char) => {
-  return char.toUpperCase().charCodeAt(0) - CHAR_CODE_OFFSET;
-}
+const charToNumber = char => char.toUpperCase().charCodeAt(0) - CHAR_CODE_OFFSET;
 
 const locatorToLatLng = (locatorString) => {
-  locatorString += 'll';  //append subsquare in case is 4 chars long...  If not, is ignored. 
+  locatorString += 'll'; // append subsquare in case is 4 chars long...  If not, is ignored.
   if (!isValidLocatorString(locatorString)) {
     throw new Error('Input is not valid locator string');
   }
@@ -21,14 +19,15 @@ const locatorToLatLng = (locatorString) => {
   const gridLat = Number.parseInt(locatorString[3]);
   const subsquareLng = (charToNumber(locatorString[4]) + 0.5) / 12;
   const subsquareLat = (charToNumber(locatorString[5]) + 0.5) / 24;
+
   return [
-    squareLat + gridLat + subsquareLat - 90, 
+    squareLat + gridLat + subsquareLat - 90,
     squareLng + gridLng + subsquareLng - 180
   ];
 };
 
-const degToRad = deg => (deg % 360) * Math.PI /180;
-const radToDeg = rad => (rad / Math.PI *180 ) % 360 ; 
+const degToRad = deg => (deg % 360) * Math.PI / 180;
+const radToDeg = rad => (rad / Math.PI *180) % 360;
 
 const bearingDistance = (from, to) => {
   const fromCoords = locatorToLatLng(from);
@@ -40,26 +39,22 @@ const bearingDistance = (from, to) => {
   const a = Math.pow(Math.sin(dLat / 2), 2) + Math.pow(Math.sin(dLon / 2), 2) * Math.cos(fromLat) * Math.cos(toLat);
   const b = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-  var az = Math.atan2((dLon) * Math.cos(fromLat) * Math.cos(toLat),
-                    Math.sin(toLat) - Math.sin(fromLat) * Math.cos(b));
-	if (az < 0) {
-		az += 2 * Math.PI;
-	}
-  return { 
-    km: b * 6371,
-    deg: radToDeg(az)
+  const y = (dLon) * Math.cos(fromLat) * Math.cos(toLat);
+  const x = Math.sin(toLat) - Math.sin(fromLat) * Math.cos(b);
+
+  let az = Math.atan2(y, x);
+
+  if (az < 0) {
+    az += 2 * Math.PI;
   }
 
+  return {
+    km: b * 6371,
+    deg: radToDeg(az)
+  };
 };
 
-
-const distance = (from, to) => {
-  let bd = bearingDistance(from, to);
-  return(bd.km);
-}
-
-
-
+const distance = (from, to) => bearingDistance(from, to).km;
 
 module.exports = {
   isValidLocatorString: isValidLocatorString,
